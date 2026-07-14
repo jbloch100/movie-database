6422bd1b6b0c6ea91e22057adc13efedaaf3d4b1
Jonathan Bloch <jonathanbloch100@gmail.com>
2026-07-13 20:17:23 -0400

    Set up React Router and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <h1>Movie Database</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <main>
+      <h1>Movie Database</h1>
+      <p>Discover popular movies and search for your favorites.</p>
+    </main>
+  );
+}
+
+export default Home;
