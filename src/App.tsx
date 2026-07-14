0cff78ae567678be71495014cea743c8e59328de
Jonathan Bloch <jonathanbloch100@gmail.com>
2026-07-13 20:43:35 -0400

    Styled navigation and app layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
+import "./App.css"
 
 function App() {
   return (
